bb2e23e42c4292f46f36c32c393e906330a1a183
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-08 18:27:59 -0300

    Relearning JS

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let js = 'amazing';
+let js = "amazing";
 let n = 40 + 8 + 23 - 10;
 
 console.log(n);
 
-if (js === 'amazing') console.log('JavaScript is Fun');
-if (js === 'amazing') console.log('JavaScript is Fun');
-if (js === 'amazing') console.log('JavaScript is Fun');
+if (js === "amazing") console.log("JavaScript is Fun");
+if (js === "amazing") console.log("JavaScript is Fun");
+if (js === "amazing") console.log("JavaScript is Fun");
+
+const func = function name(params) {
+	console.log("Teste 1234");
+};
+
+func();
