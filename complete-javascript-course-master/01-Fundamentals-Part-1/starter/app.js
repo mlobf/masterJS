c25f8e230c81b7b7e4140203a59eeed08acc2bdf
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-10 18:55:32 -0300

    Lesson 10

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/app.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/app.js
@@ -1,4 +1,26 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is Fun");
-
 console.log(40 + 33 + 2);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+
+console.log(firstName);
+
+//To use a reserved name to name something, its possible put a _ $ in front of variable name.
+// function -> Error
+// _function -> ok
+// $function -> ok
+
+// Do not start a variable with an Upperletter
+
+// Variables constants shall be written in uppercase
+
+let PI = 3.1421;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myCurrentJob);
+console.log(myFirstJob);
