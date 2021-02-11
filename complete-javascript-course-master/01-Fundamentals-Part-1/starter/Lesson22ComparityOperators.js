cebb80108a7de950873271fc9037c7c91670b35b
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-11 17:19:25 -0300

    End of lesson 22

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/Lesson22ComparityOperators.js
@@ -0,0 +1,30 @@
+// Lets compare them all!!!
+
+const age = 18;
+
+if (age === 18) {
+	console.log("Yes, 18 allowed !");
+}
+
+// Important
+// the == perform type coercion.
+
+if (age == "18") {
+	console.log("yes, 18 allowed ! == ");
+}
+
+if (age === "18") {
+	console.log("yes, 18 allowed ! === ");
+}
+// As matter to Clean Code, avoid == as much you can.
+
+const favorite = prompt("Whats your favorite number ?");
+
+console.log(favorite);
+console.log(typeof favorite);
+if (favorite == 23) {
+	console.log(`Cool! The number is 23 ${typeof favorite}`);
+}
+
+
+
