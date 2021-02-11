dd9bfa5e2f07a662f91632ac72f551b48e89a6d2
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-10 22:05:01 -0300

    Lesson 14 Basic Operators

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/app.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/app.js
@@ -4,9 +4,9 @@ console.log(40 + 33 + 2);
 console.log("Jonas");
 console.log(23);
 
-let firstName = "Jonas";
+//let firstName = "Jonas";
 
-console.log(firstName);
+//console.log(firstName);
 
 //To use a reserved name to name something, its possible put a _ $ in front of variable name.
 // function -> Error
@@ -45,3 +45,37 @@ console.log(age);
 
 meuNome = "marcos";
 console.log(meuNome);
+
+// Operators Lesson 14
+const now = 2037;
+const ageJonas = now - 1981;
+const ageSarah = now - 2018;
+
+console.log(ageJonas * 2, ageSarah / 2, 3 ** 5);
+
+// How to concatenate two o more strings.
+
+const firstName = "Marcos";
+const lastName = "Filho";
+
+console.log(firstName + " " + lastName);
+
+let x = 15;
+x += 10;
+console.log(x);
+
+x++; // means -> x = x + 1
+x--; // means -> x = x - 1
+x--; // means -> x = x - 1
+
+console.log(x);
+
+// Comparison operators
+
+console.log(ageJonas > ageSarah);
+
+console.log(ageSarah >= 18);
+
+const resultAgeSarah = ageSarah >= 18;
+
+console.log(resultAgeSarah);
