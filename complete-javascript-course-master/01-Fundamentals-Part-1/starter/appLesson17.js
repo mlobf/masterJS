5502e72dffc8bcf9196d0d158970ca33b965c3ec
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-11 10:09:35 -0300

    End Lesson 17 template Strings

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/appLesson17.js
@@ -0,0 +1,22 @@
+// Strings and Literal
+
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas =
+	"I'am " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+
+console.log(jonas);
+
+// Template Literals
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job} !`;
+
+console.log(jonasNew);
+console.log("A new string ....... \n and other line here too....");
+
+console.log(`Add a new line without using back slash n...... 
+now this is easier !!!
+Bye bye`);
