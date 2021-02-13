9899edd2ab26ac76d82f353eb016c1cd7b74581d
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-13 15:50:54 -0300

    Lesson 28 Ternary Operators

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/Lesson28CondicionalOperator.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/Lesson28CondicionalOperator.js
@@ -1,1 +1,26 @@
-// Conditional  Operators Ternary Operators.
+// Conditional  Operators Ternary Operators. ? :
+// Ternary has 3 Parts. Condition/The IF/ The Else
+
+const age = 22;
+
+/*
+age >= 18
+	? console.log("I like to drink wine!")
+	: console.log("I like to drink water");
+*/
+
+const drink = age >= 18 ? "Wine" : "Water";
+
+console.log(drink);
+
+// The same thing using if statement
+let drink2;
+if (age >= 18) {
+	drink2 = "wine";
+} else {
+	drink2 = "water";
+}
+
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "Wine" : "Water"}`);
