1627198ed027a8004550b505a48cdef47928dd43
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-13 14:07:21 -0300

    Lesson 27 Statements and Expressions

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/Lesson27.js
@@ -0,0 +1,15 @@
+// Statements and Expressions.
+
+// Expression is a piece of code that produces a value. Like the words.
+
+3 + 4;
+true && false && !false;
+1991;
+
+// Statement is a big piece of code that is executed and witch does not produce
+//  a value on itself.
+//  Like a sentence.
+if (23 > 10) {
+	const str = "23 is bigger than 10!!";
+}
+// This upper block of code does not produce.
