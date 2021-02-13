fec3f2f7ed7b402b836849532064c0ef300f306c
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-13 18:52:07 -0300

    Lesson 33 Functions

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/Lesson33Function.js
@@ -0,0 +1,24 @@
+"use strict";
+// Functions
+
+function logger() {
+	console.log("My name is Marcos");
+}
+
+logger();
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+	console.log(apples, oranges);
+	const juice = `Juice with ${apples} apples and
+   ${oranges} oranges`;
+	return juice;
+}
+
+const appleJuice = fruitProcessor(0, 5);
+console.log(appleJuice);
+
+
+
