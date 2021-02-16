1eacab2400a1032bc3ede76ea13ad912e775897e
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-16 12:04:33 -0300

    Lesson 46 for

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/Lesson46ForLoops.js
@@ -0,0 +1,8 @@
+"use strict";
+// For Loops in JS
+const rep = 99;
+const phrase = `Lifting weight repetitions`;
+
+for (let count = 0; count < rep; count++) {
+	console.log(`${phrase} at ${count} times`);
+}
