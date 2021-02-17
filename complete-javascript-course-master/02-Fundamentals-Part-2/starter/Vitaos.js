2d69588c44866a90ada2aee32ed1df6eb48b1501
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-16 22:04:08 -0300

    Vitao Commit

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/Vitaos.js
@@ -0,0 +1,23 @@
+"use strict";
+const test = "sdsafdfdssfsfasfd";
+let victor = 2;
+const victorArray = [];
+
+/*
+if (victor === 2) {
+	console.log(victor > 3 ? 4 : 8);
+}
+
+for (let i = 0; i < 10; i++) {
+	console.log(victor);
+}
+*/
+
+const marcos = {
+	nome: "Marcos",
+
+	printNome: function () {
+		return `O meu nome é ${this.nome}`;
+	},
+};
+console.log(marcos.printNome());
