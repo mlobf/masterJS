240489adc381a0922311446703ab18a6c53ac035
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-18 18:57:48 -0300

    Lesson 73

--- complete-javascript-course-master/05-Guess-My-Number/starter/Lesson73EventOnClick/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/Lesson73EventOnClick/script.js
@@ -1,14 +1,13 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
-
 document.querySelector('.message').textContent = 'Correct Number!!';
-
 document.querySelector('.number').textContent = '13';
-
 document.querySelector('.score').textContent = '20';
-
 document.querySelector('.guess').value = 23;
-
 console.log(document.querySelector('.guess').value);
+*/
 
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
