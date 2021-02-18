52a2953a1ba5b013bfedd82e654f1fb71a40e088
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-18 18:21:00 -0300

    Lesson 72 How to use the querySelect

--- complete-javascript-course-master/05-Guess-My-Number/starter/Lesson72 SelectingAndChangingDOM/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/Lesson72 SelectingAndChangingDOM/script.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!!';
+
+document.querySelector('.number').textContent = '13';
+
+document.querySelector('.score').textContent = '20';
+
+document.querySelector('.guess').value = 23;
+
+console.log(document.querySelector('.guess').value);
+
