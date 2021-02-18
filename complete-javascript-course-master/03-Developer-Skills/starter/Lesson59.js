cac3d1c9aee1920904fe9054a24c7c1d72fca320
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-17 21:59:16 -0300

    End Lesson 59

--- complete-javascript-course-master/03-Developer-Skills/starter/Lesson59.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/Lesson59.js
@@ -2,26 +2,20 @@
 // Calculate the temperature amplitude;
 // Be aware about sensors error.
 
-const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
-// Amplitude = Max value - Min value.
-// Create a 3th array with to combined values of the last Arrays
+const temperatures1 = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+const temperatures2 = [3, -24, -6, -1, "error", 9, 13, 171, 15, 14, 9, 5];
 
-const add = (arr1, arr2) => {
-	const arrUnion = [];
-	for (let i = 0; i < arr2.length; i++) {
-    arr1.push(arr2[i])
-  }
-};
+const temperature = temperatures1.concat(temperatures2);
 
 const amplitude = (temperature) => {
 	const amp = [];
-	for (let i = 0; i <= temperatures.length; i++) {
-		if (typeof temperatures[i] === "number") {
-			amp.push(temperatures[i]);
+	for (let i = 0; i <= temperature.length; i++) {
+		if (typeof temperature[i] === "number") {
+			amp.push(temperature[i]);
 		}
 	}
 	const ampValue = Math.max(...amp) - Math.min(...amp);
 	console.log(ampValue);
 };
 
-amplitude(temperatures);
+amplitude(temperature);
