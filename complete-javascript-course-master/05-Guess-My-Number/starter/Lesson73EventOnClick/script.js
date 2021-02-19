8ae3cd2d3575d1244833b56a98ca10c9307f25a7
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-19 19:15:44 -0300

    teste

--- complete-javascript-course-master/05-Guess-My-Number/starter/Lesson73EventOnClick/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/Lesson73EventOnClick/script.js
@@ -6,6 +6,7 @@ document.querySelector('.number').textContent = '13';
 document.querySelector('.score').textContent = '20';
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+test;
 */
 
 document.querySelector('.check').addEventListener('click', function () {
