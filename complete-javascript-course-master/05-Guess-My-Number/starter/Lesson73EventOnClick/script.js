da35d939d1e25cece6a0fd571122b138d10b48fe
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-19 18:54:07 -0300

    Lesson 73 Click Events

--- complete-javascript-course-master/05-Guess-My-Number/starter/Lesson73EventOnClick/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/Lesson73EventOnClick/script.js
@@ -9,5 +9,10 @@ console.log(document.querySelector('.guess').value);
 */
 
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number !!!';
+  }
 });
