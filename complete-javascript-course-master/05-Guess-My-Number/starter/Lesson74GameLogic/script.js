0f8ebf172ef5b713604b94ff315c7a9bc2465291
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-20 10:32:05 -0300

    Lesson 74 Game Logic Beg

---
+++ complete-javascript-course-master/05-Guess-My-Number/starter/Lesson74GameLogic/script.js
@@ -0,0 +1,19 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!!';
+document.querySelector('.number').textContent = '13';
+document.querySelector('.score').textContent = '20';
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+test;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number !!!';
+  }
+});
