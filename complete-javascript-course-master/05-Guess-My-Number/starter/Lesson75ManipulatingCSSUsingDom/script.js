e56bd93c8000d6c92259c77be0bff3ff070e8f6b
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-20 12:23:05 -0300

    End Lesson 75

--- complete-javascript-course-master/05-Guess-My-Number/starter/Lesson75ManipulatingCSSUsingDom/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/Lesson75ManipulatingCSSUsingDom/script.js
@@ -18,12 +18,13 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
 
-  // Add you lose situation.
-  // if your score is <= 0 you lose the game
+  // When there is no input number
   if (!guess) {
     document.querySelector('.message').textContent = 'No number !!!';
     myScore--;
     document.querySelector('.score').textContent = myScore;
+
+    // When guess number is greater then secret number
   } else if (guess > secretNumber) {
     if (myScore > 1) {
       document.querySelector('.message').textContent = 'Your number is higher ';
@@ -33,6 +34,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game !!!';
       document.querySelector('.score').textContent = 0;
     }
+    // When guess number is lower than secret number
   } else if (guess < secretNumber) {
     if (myScore > 1) {
       document.querySelector('.message').textContent = 'Your number is lower !';
@@ -42,7 +44,10 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game !!!';
       document.querySelector('.score').textContent = 0;
     }
+    // When the player guess the secret number.
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number !!!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   }
 });
