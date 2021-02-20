4e9d8a71ebff587a803943c496d31ed485e858fe
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-20 15:39:18 -0300

    Lesson78 Refactoring End

--- complete-javascript-course-master/05-Guess-My-Number/starter/Lesson78Refactoring/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/Lesson78Refactoring/script.js
@@ -1,26 +1,18 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!!';
-document.querySelector('.number').textContent = '13';
-document.querySelector('.score').textContent = '20';
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-test;
-*/
-
-// Implement High score  .
-//
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let myScore = 20;
 let myHighScore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 // When the button Again is clicked
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.score').textContent = 20;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
   //myScore = 0;
@@ -38,33 +30,23 @@ document.querySelector('.check').addEventListener('click', function () {
 
   // When there is no input number
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number !!!';
+    displayMessage('No number input');
     myScore--;
     document.querySelector('.score').textContent = myScore;
-
-    // When guess number is greater then secret number
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (myScore > 1) {
-      document.querySelector('.message').textContent = 'Your number is higher ';
+      displayMessage(
+        guess > secretNumber ? 'Your number is higher' : 'Your number is lower'
+      );
       myScore--;
       document.querySelector('.score').textContent = myScore;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game !!!';
-      document.querySelector('.score').textContent = 0;
-    }
-    // When guess number is lower than secret number
-  } else if (guess < secretNumber) {
-    if (myScore > 1) {
-      document.querySelector('.message').textContent = 'Your number is lower !';
-      myScore--;
-      document.querySelector('.score').textContent = myScore;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game !!!';
+      displayMessage('You lost the game !!!');
       document.querySelector('.score').textContent = 0;
     }
     // When the player guess the secret number.
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number !!!';
+    displayMessage('Correct Number !!!');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
