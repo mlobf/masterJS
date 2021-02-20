517714fb7d3efead36a5b8ede33c43426bc1f50a
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-20 11:24:00 -0300

    Lesson 74 GameLogic BAsic

--- complete-javascript-course-master/05-Guess-My-Number/starter/Lesson74GameLogic/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/Lesson74GameLogic/script.js
@@ -9,11 +9,30 @@ console.log(document.querySelector('.guess').value);
 test;
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let myScore = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number !!!';
+    myScore--;
+    document.querySelector('.score').textContent = myScore;
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number !!!';
+
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Your number is higher ';
+    myScore--;
+    document.querySelector('.score').textContent = myScore;
+
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Your number is lower !';
+    myScore--;
+    document.querySelector('.score').textContent = myScore;
   }
 });
