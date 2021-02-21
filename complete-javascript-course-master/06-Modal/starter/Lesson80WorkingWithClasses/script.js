e2e7e4dab12dca75a086a8a733b448e7e79fdeee
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-21 12:29:43 -0300

    Lesson80WorkingWithClasses

--- complete-javascript-course-master/06-Modal/starter/Lesson80WorkingWithClasses/script.js
+++ complete-javascript-course-master/06-Modal/starter/Lesson80WorkingWithClasses/script.js
@@ -3,12 +3,24 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-
 // To Select All Elements use .querySelectorAll
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnOpenModal);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnOpenModal.length; i++) {
-  console.log(btnOpenModal[i].textContent);
+  btnOpenModal[i].addEventListener('click', openModal);
 }
+
+// At closeModal, do not use with (). We want to execute the function only
+//    after the Click event, not before.
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
