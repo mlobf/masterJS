ba8429799641f2e8d5bdb0ac92475651f63c8a79
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-22 17:25:02 -0300

    Lesson83RollingDice

--- complete-javascript-course-master/07-Pig-Game/starter/Lesson83RollingTheDice/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/Lesson83RollingTheDice/script.js
@@ -2,10 +2,38 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+const current0el = document.getElementById('current--0');
+const current1el = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let currentScore = 0;
 
 // Starting Conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Implement the Rolling Dice Functionality
+btnRoll.addEventListener('click', function () {
+  // 1 Generation Random Dice Roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2 Display the Dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  console.log(dice);
+
+  // 3 Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score.
+    currentScore += dice;
+    current0el.textContent = currentScore; // Change Later
+  } else {
+    // Switch to next player
+    currentScore = 0;
+  }
+});
