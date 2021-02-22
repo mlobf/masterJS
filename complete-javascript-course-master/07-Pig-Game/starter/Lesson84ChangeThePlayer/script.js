81b97fe971dd5fa53307907ba4368f8191d561bf
mlobf <marcoslemeborbafilho@gmail.com>
2021-02-22 18:29:48 -0300

    Lesson84SwichingActivePlayer

--- complete-javascript-course-master/07-Pig-Game/starter/Lesson84ChangeThePlayer/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/Lesson84ChangeThePlayer/script.js
@@ -1,16 +1,24 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+
 const current0el = document.getElementById('current--0');
 const current1el = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
+
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+const scores = [0, 0];
+
 let currentScore = 0;
+let activePlayer = 0;
 
 // Starting Conditions
 score0El.textContent = 0;
@@ -31,9 +39,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score.
     currentScore += dice;
-    current0el.textContent = currentScore; // Change Later
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
     // Switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
     currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
