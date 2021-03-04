c4a8e2bbf96db9bf5e8f6e39c98e5f8c0f828d66
mlobf <marcoslemeborbafilho@gmail.com>
2021-03-03 22:22:27 -0300

    Lesson 113 Looping Objects

---
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/Lesson113 Looping Objects/script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openHours = {
+  [weekDays[0]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekDays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [`day-${2 + 4}`]: {
+    open: 0,
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  // ES6 Way
+  openHours, // The name Must be the same.
+
+  // Now an easy way to do the things.
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex]], this.mainMenu[this.mainIndex];
+  },
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received!! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
+        will be delivered to ${address} at ${time}`
+    );
+  },
+};
+//console.log('------- BEG +++++++++');
+
+//ES2020 Solution
+//console.log(restaurant.openHours.mon?.open); // Only if mon exist, the open property will be touched
+
+//console.log('+++++++++ END ---------');
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  //console.log(day);
+  const open = restaurant.openHours[day]?.open ?? 'closed';
+  //console.log(`On ${day} we open at ${open}`);
+}
+
+// Using optional chaining on Methods
+//console.log(restaurant.orderDelivery?.(1, 1) ?? 'Method does not exist ');
+//console.log(restaurant.orderRissoto?.(0, 1) ?? 'Method does not exist ');
+
+// Using even on arrays
+// Check if this arrays is empty
+const users = [{ name: 'Jonas' }, { email: 'hello@gmail.com' }, { age: 38 }];
+
+console.log(users[0]?.name ?? 'This dude does not exist');
