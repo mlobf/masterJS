23a6e1ebc36a2d99fe67fc03034124d75338f972
mlobf <marcoslemeborbafilho@gmail.com>
2021-03-05 21:40:46 -0300

    Recap lesson

---
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/Lesson105TheSpreadOperator/rescript.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex]], this.mainMenu[this.mainIndex];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received!! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
+        will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+const arr = [7, 8, 9];
+const newArr = [4, 5, 6, ...arr];
+console.log(newArr);
+
+const newMenu = [...restaurant.mainMenu];
+console.log(newMenu);
