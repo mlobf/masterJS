11c3a7d2720539de497a9c5eb9e8e00af238cece
mlobf <marcoslemeborbafilho@gmail.com>
2021-03-10 22:19:59 -0300

    Lesson 117 Maps Iterators Done

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/Lesson117MapsIteration/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/Lesson117MapsIteration/script.js
@@ -1,48 +1,55 @@
 'use strict';
+const weekDays = ['Sun', 'Mon', 'Thu', 'Wed', 'Qui', 'Fri', 'Sat'];
 
-// New Data Structure
-// Maps
+const openHours = {
+  [weekDays[0]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekDays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekDays[6]]: {
+    open: 0,
+    close: 24,
+  },
+};
 
-console.log('--------------- BEG --------------------');
+const question = new Map([
+  ['Question', 'What is the best programming language in the world'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', '3'],
+  [true, 'Correct'],
+  [false, 'Try again'],
+]);
 
-const rest = new Map();
-// How to add a element to a Map
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze Italia');
-rest.set(2, 'Lisbon, Portugal ');
+//console.log(question);
 
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open')
-  .set(false, 'We are closed');
-// To create a arr as key, first you must create a const arr and use this arr
-//    as key to a new Map element
-const arr = [1, 2];
-rest.set(arr, 'Test');
-// Using a Object as Key on Map
-rest.set(document.querySelector('h1'), 'Heading');
+// Easy way to convert Object to Map
+//const hourMap = new Map(Object.entries(openHours));
+//console.log(hourMap);
+// Quiz App
+// Iteration on Maps
+/*
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key} : ${value}`);
+  }
+}
+*/
+const answer = Number(prompt('Your answer is ...'));
+if (answer === 3) {
+  console.log('You Win');
+} else {
+  console.log('You Lose');
+}
+//console.log(question.get(question.get(3) === answer));
 
-console.log(rest.get('name'));
-console.log(rest.get(true));
-console.log(rest.get(1));
-
-const time = 21;
-// this is how we use Boolean as Map keys
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
-
-// Check if this map contain a certain key
-console.log(rest.has('categories'));
-// Delete some element on Maps
-console.log(rest.delete(2));
-// The size propertyA
-console.log(rest.size);
-// How to clear, remove all elements inside map.
-//console.log(rest.clear);
-
-console.log(rest.get(arr));
-
-console.log(rest);
-
-console.log('--------------- END --------------------');
+// Convert Map to Array
+console.log([...question]);
+console.log(question.entries()); // Keys and Values
+console.log(question.keys()); // Only Keys
+console.log(question.values()); // Only Values
