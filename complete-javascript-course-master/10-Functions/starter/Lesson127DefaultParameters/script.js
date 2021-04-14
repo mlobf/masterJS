ea6b62d3e554e775688ee71a454c758904c412f7
mlobf <marcoslemeborbafilho@gmail.com>
2021-04-14 18:07:08 -0300

    Lesson 127 Functions Default Parameters

---
+++ complete-javascript-course-master/10-Functions/starter/Lesson127DefaultParameters/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const bookings = [];
+
+const createBooking = (
+  flightNum = 'A1',
+  numPassengers = 1,
+  price = 199 * numPassengers
+) => {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('LH123');
+createBooking('AC120', 1000, 1200);
