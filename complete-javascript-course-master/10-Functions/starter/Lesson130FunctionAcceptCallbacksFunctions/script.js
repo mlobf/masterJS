57413ef0d91cd226dfa66fcefedbdcd8d2bf37dc
mlobf <marcoslemeborbafilho@gmail.com>
2021-04-19 19:17:35 -0300

    End Lesson 130 Functions and Call Backs functions

--- complete-javascript-course-master/10-Functions/starter/Lesson130FunctionAcceptCallbacksFunctions/script.js
+++ complete-javascript-course-master/10-Functions/starter/Lesson130FunctionAcceptCallbacksFunctions/script.js
@@ -1,9 +1,12 @@
 'use strict';
 
+// Creating functions that accepts others functions as params
+
 const oneWord = function (str) {
   return str.replace(/ /g, '').toLowerCase();
 };
-const myName = 'Marcos Leme De Oliveira Borba Filho';
+
+//const myName = 'Marcos Leme De Oliveira Borba Filho';
 
 //console.log(oneWord(myName));
 
@@ -12,9 +15,22 @@ const upperFirstWord = function (str) {
   return [first.toUpperCase(), ...other].join('');
 };
 
-// Higher Order function
+// Higher-order function
+//  That uses fn as params
+//    where fn can be any function
+
 const transformer = function (str, fn) {
   console.log(`Transform string: ${fn(str)}`);
+  console.log(`Transformed by ${fn.name}`);
 };
 
 transformer('JavaScript is the Best!!', upperFirstWord);
+transformer('JavaScript is the Best!!', oneWord);
+
+const hight5 = () => {
+  console.log(' Hi hight five bro!!');
+};
+
+// Call back functions allow us up abstraction.
+// Attention on the arch
+['John', 'Marcos', 'Lin'].forEach(hight5);
