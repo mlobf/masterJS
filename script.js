240738ad6bab67dc28196167e1bc56646c12413f
mlobf <marcoslemeborbafilho@gmail.com>
2022-05-18 21:28:45 -0300

    isdfasd

--- script.js
+++ script.js
@@ -8,7 +8,14 @@ if (js === "amazing") console.log("JavaScript is Fun");
 if (js === "amazing") console.log("JavaScript is Fun");
 
 const myFunc = function name(params) {
-	console.log("Teste 1234");
+  console.log("Teste 1234");
 };
 
 myFunc();
+// Teste Git New
+
+/*
+git config --global user.email "marcoslemeborbafilho@gmail.com"
+
+git config --global user.name "mlobf"
+*/
